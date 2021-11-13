988c91e889fbe745aadc7aca5192741deb953bed
Koulinn <drdverzola@gmail.com>
2021-11-13 10:10:51 +0000

    added filter to display chat messages properly

--- src/components/Chat/ChatColumn.jsx
+++ src/components/Chat/ChatColumn.jsx
@@ -4,6 +4,8 @@ import ChatHeader from './ChatHeader';
 import ChatBottom from './ChatBottom';
 
 function ChatColumn({ currentUserOnChat, currentMessageHistory }) {
+  const filteredDisplayedMessages = currentMessageHistory.filter(c=> c.roomID.toString() === currentUserOnChat._id.toString())
+
   return (
     <div className="col-8 chat-wrapper px-0">
       <ChatHeader
@@ -11,7 +13,7 @@ function ChatColumn({ currentUserOnChat, currentMessageHistory }) {
       />
       <div className="scrollable-Area p-3 row mx-0">
         <div className="w-100">
-          {currentMessageHistory.map(m => <ChatCard
+          {filteredDisplayedMessages.map(m => <ChatCard
             key={m._id}
             message={m}
             user={m.senderRole === 'user' ? true : false}
